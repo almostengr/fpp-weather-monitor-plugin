a6b3eb79ab8a2ae67d677efd8dacca9754da19cc
Almost Engineer <bitsecondal@gmail.com>
2023-03-26 17:21:31 -0500

    #4 create settings page; updated existing scripts

--- about.php
+++ about.php
@@ -2,14 +2,13 @@
   <fieldset style="padding: 10px; border: 2px solid #000;">
     <legend>Weather Monitor Plugin Info</legend>
     <div style="overflow: hidden; padding: 10px;">
-      <div>
-        <div id='credits'>
-          <b>Template Plugin Developed By:</b><br /><br />
-          Kenny Robinson (almostengr)<br /><br />
-          <a href='https://github.com/almostengr/fpp-weather-monitor-plugin'>Git Repository</a><br>
-          <a href='https://github.com/almostengr/fpp-weather-monitor-plugin/issues'>Bug Reporter</a><br>
-          <br />
-        </div>
+      <div id='credits'>
+        <b>Template Plugin Developed By:</b><br /><br />
+        Kenny Robinson (almostengr)<br /><br />
+        <a href='https://github.com/almostengr/fpp-weather-monitor-plugin'>Git Repository</a><br>
+        <a href='https://github.com/almostengr/fpp-weather-monitor-plugin/issues'>Bug Reporter</a><br>
+        <br />
       </div>
+    </div>
   </fieldset>
 </div>
--- scripts/postStart.sh
+++ scripts/postStart.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-echo "Running fpp-weather-monitor-plugin PostStart Script"
-
+# echo "Running fpp-weather-monitor-plugin PostStart Script"
+/usr/bin/php /home/fpp/media/plugins/weather-monitor/weather_monitor.php &
