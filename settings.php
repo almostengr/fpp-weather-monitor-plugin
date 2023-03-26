b3be193e9b14354a397ae2b7bad0eca081f848db
Falcon Player <fpp@FPP>
2023-03-26 18:10:50 -0500

    #4 updated settings and plugin name

--- settings.php
+++ settings.php
@@ -2,3 +2,20 @@
 
 include_once '/opt/fpp/www/common.php';
 
+// todo reference
+?>
+
+<html>
+<head>
+</head>
+<body>
+<div>
+  <div class="form-group">
+<label for="locationCallSign">Location Call Sign</label>
+<input class="form-control" type="text" name="locationCallSign" value="KMGM" required="required" />
+</div>
+</div>
+
+</body>
+</html>
+
