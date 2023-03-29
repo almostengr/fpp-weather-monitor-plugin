1559878dc512d4830c3a5d397d3563a584ff355f
Kenny Robinson <bitsecondal@gmail.com>
2023-03-29 16:29:09 -0500

    #4 updated to procedural style

--- settings.php
+++ settings.php
@@ -2,11 +2,21 @@
 
 <body>
   <?php
-  require_once './source/services.inc';
+  // require_once './source/services.inc';
+  require_once '/home/fpp/media/plugins/fpp-weather-monitor-plugin/common.inc';
 
-  $settingService = new SettingsFormService();
-  if (!empty($_POST)) {
-    $settingService->saveSettings($_POST[NWS_WEATHER_STATION_ID], $_POST[EMAIL_ADDRESS_SETTING], $_POST[WEATHER_DESCRIPTIONS], $_POST[MAX_WIND_SPEED], $_POST[MAX_GUST_SPEED]);
+  // $settingService = new SettingsFormService();
+  // if (!empty($_POST)) {
+  //   $settingService->saveSettings($_POST[NWS_WEATHER_STATION_ID], $_POST[EMAIL_ADDRESS_SETTING], $_POST[WEATHER_DESCRIPTIONS], $_POST[MAX_WIND_SPEED], $_POST[MAX_GUST_SPEED]);
+  // }
+
+  if (!empty($_POST))
+  {
+      $this->setWeatherStationIdSetting($_POST[NWS_WEATHER_STATION_ID]);
+      $this->setEmailAddressSetting($_POST[EMAIL_ADDRESS_SETTING]);
+      $this->setWeatherTextDescriptionsSetting($_POST[WEATHER_DESCRIPTIONS]);
+      $this->setMaxWindSpeedSetting($_POST[MAX_WIND_SPEED]);
+      $this->setMaxGustSpeedSetting($_POST[MAX_GUST_SPEED]);
   }
   ?>
 
@@ -20,21 +30,21 @@
       <div class="col-md-4">NWS Weather Station ID</div>
       <div class="col-md">
         <input class="" type="text" name="<?php echo NWS_WEATHER_STATION_ID; ?>"
-          value="<?php echo $settingService->getWeatherStationId(); ?>" required="required" />
+          value="<?php echo $settingService->getWeatherStationIdSetting(); ?>" required="required" />
       </div>
     </div>
     <div class="row">
       <div class="col-md-4">Email Address</div>
       <div class="col-md">
         <input class="" type="text" name="<?php echo EMAIL_ADDRESS_SETTING; ?>"
-          value="<?php echo $settingService->getEmailAddress(); ?>" required="required" />
+          value="<?php echo $settingService->getEmailAddressSetting(); ?>" required="required" />
       </div>
     </div>
     <div class="row">
       <div class="col-md-4">Weather Text Descriptions</div>
       <div class="col-md">
         <input type="text" name="<?php echo WEATHER_DESCRIPTIONS; ?>"
-          value="<?php echo $settingService->getWeatherTextDescriptions(); ?>" />
+          value="<?php echo $settingService->getWeatherTextDescriptionsSetting(); ?>" />
       </div>
     </div>
 
@@ -46,7 +56,7 @@
       </div>
       <div class="col-md">
         <input class="" type="number" name="<?php echo MAX_WIND_SPEED; ?>"
-          value="<?php echo $settingService->getMaxWindSpeed(); ?>" />
+          value="<?php echo $settingService->getMaxWindSpeedSetting(); ?>" required="required" />
       </div>
     </div>
     <div class="row">
@@ -56,7 +66,7 @@
       </div>
       <div class="col-md">
         <input class="" type="number" name="<?php echo MAX_GUST_SPEED; ?>"
-          value="<?php echo $settingService->getMaxGustSpeed(); ?>" />
+          value="<?php echo $settingService->getMaxGustSpeedSetting(); ?>" required="required" />
       </div>
     </div>
 
