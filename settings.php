c8b2e10b4a256cc24a7855e5abbdcef0a929ccec
Almost Engineer <bitsecondal@gmail.com>
2023-03-31 21:25:34 -0500

    #4 updated settings page

--- settings.php
+++ settings.php
@@ -23,85 +23,73 @@ if (!empty($_POST)) {
     }
   }
   ?>
+
   <form method="post">
-    <div>
-      <h2>Donate</h2>
-      <div class="">Donate</div>
-      <div class="">
+    <div class="row my-2">
+      <div class="col-md-2 text-center">Donate</div>
+      <div>
         Enjoy using this plugin? Please make a donation to support the future of this plugin.
       </div>
     </div>
 
-<div class="row py-2">
-<div class="col-md-2 text-center">NWS Weather Station</div>
-<div>
-<input type="text">
-<div class="text-muted">
-Help information about the setting.
-</div>
-</div>
-</div>
-
-<div class="form-group">
-<label>NWS Weather Station</label>
-<input type="text">
-<div>This is some information about the weather station</div>
-</div>
-
-    <div>
-      <h2 class="pt-2">General</h2>
-      <div class="">NWS Weather Station ID</div>
-      <div class="">
+    <div class="row my-2">
+      <div class="col-md-2 text-center">NWS Weather Station ID</div>
+      <div>
         <input class="" type="text" name="<?php echo NWS_WEATHER_STATION_ID; ?>"
           value="<?php echo $settingService->getSetting(NWS_WEATHER_STATION_ID); ?>" required="required" />
-        <div>
+        <div class="text-muted">
           If left blank, station ID will automatically be populated from the GPS coordinates entered at Status/Control >
           FPP Settings > System.
         </div>
       </div>
-      <div class="">Email Address</div>
-      <div class="">
+    </div>
+
+    <div class="row my-2">
+      <div class="col-md-2 text-center">Email Address</div>
+      <div>
         <input class="" type="text" name="<?php echo EMAIL_ADDRESS_SETTING; ?>"
           value="<?php echo $settingService->getSetting(EMAIL_ADDRESS_SETTING); ?>" required="required" />
-        <div>
+        <div class="text-muted">
           Enter the email address that will be used to identify your API calls made to the National Weather Service API.
           If ther are issues or concerns with the APIs calls that are made, you will be notified by the NWS at the email
           entered.
         </div>
       </div>
     </div>
-    <div>
-      <div class="">Weather Text Descriptions</div>
-      <div class="">
+
+    <div class="row my-2">
+      <div class="col-md-2 text-center">Weather Text Descriptions</div>
+      <div>
         <input type="text" name="<?php echo WEATHER_DESCRIPTIONS; ?>"
           value="<?php echo $settingService->getSetting(WEATHER_DESCRIPTIONS); ?>" />
+        <div class="text-muted">
+        </div>
       </div>
     </div>
 
-    <div>
-      <h2 class="pt-2">Wind</h2>
-   </div>
-   <div class="pb-2">
-      <div class="">
+    <div class="row my-2">
+      <div class="col-md-2 text-center">
         Max Wind Speed
         <?php echo $settingService->getSpeedUnitText(); ?>
       </div>
-      <div class="">
+      <div>
         <input class="" type="number" name="<?php echo MAX_WIND_SPEED; ?>"
           value="<?php echo $settingService->getSetting(MAX_WIND_SPEED); ?>" required="required" />
-        <div>
+        <div class="text-muted">
           Enter the wind speed that if exceeded, your show will be stopped by the monitor.
         </div>
       </div>
+    </div>
 
-      <div class="">
+    <div class="row my-2">
+      <div class="col-md-2 text-center">
         Max Gust Speed
         <?php echo $settingService->getSpeedUnitText(); ?>
       </div>
-      <div class="">
+      <div>
         <input class="" type="number" name="<?php echo MAX_GUST_SPEED; ?>"
           value="<?php echo $settingService->getSetting(MAX_GUST_SPEED); ?>" required="required" />
-        <div>
+        <div class="text-muted">
           Enter the gust speed, that if exceeded, your show will be stopped by the monitor.
         </div>
       </div>
